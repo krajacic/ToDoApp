5d10b3d5ff9a39aa7ac5dade8d343121d73b9ab8
Jurica Krajačić <krajacic_jurica@yahoo.com>
2015-11-27 12:55:38 +0100

    Added onClick property to button in activity_main.xml Define method "addTodoItem" (because addTodoItem is ID of onClick Property)

--- app/src/main/java/cawabanga/com/todoapp/MainActivity.java
+++ app/src/main/java/cawabanga/com/todoapp/MainActivity.java
@@ -2,8 +2,9 @@ package cawabanga.com.todoapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -32,6 +33,13 @@ public class MainActivity extends AppCompatActivity {
         listViewID.setAdapter(itemsAdapter);
         items.add("First Item");
         items.add("Second Item");
+
+    }
+
+    public void addTodoItem(View v){ //addTodoItem is onClick id from Button!!!!!*****
+        EditText textBoxID = (EditText) findViewById(R.id.text_boxID);
+        itemsAdapter.add(textBoxID.getText().toString()); //Adding text from textBox to items Adapter
+        textBoxID.setText("");
 
     }
 
